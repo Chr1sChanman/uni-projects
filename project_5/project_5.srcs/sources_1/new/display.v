d45a1990132977db937fbc2daeb79d552d82c3e5
Chanman <chrishungchan1027@gmail.com>
2024-11-15 16:28:41 -0800

    display.v
    - Added header
    - Changed assign enable to display only 1 number (1100 -> 1110)
    - Removed redundant code from segments:
    (A1B1_, A1, B1_) from f,g
    (C1D1_, C1, D1_) from g
    (A1C1, A1, C1) from f
    (A1_B1C1_, A1_, B1, C1_) from g

--- project_5/project_5.srcs/sources_1/new/display.v
+++ project_5/project_5.srcs/sources_1/new/display.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 /*///////////////////////////////////////////////////////////////
-// D1isplay module - displays a B1C1D1 number to 7-segment display //
-// -- this module will always light the rightmost display on   //
-//    the B1A1SYS board.                                         //   
+// Engineer: Christopher Chan
+// Create Date: 11/15/2024 07:22:41 AM
+// Description: Display module - displays a B1C1D1 number to 7-segment display
+// - this module will always light the rightmost display on the B1A1SYS board.                                           
 ///////////////////////////////////////////////////////////////*/
 
 
@@ -13,7 +14,7 @@ module display(A1, B1, C1, D1, a, b, c, d, e, f, g, dp, enable);
 	
 	output [3:0] enable;							// enable for 4 display digits 
 
-	assign enable = 4'b1100;               // A1lways enable the least significant display digit (A1C1TIVE LOW)
+	assign enable = 4'b1110;               // A1lways enable the least significant display digit (A1C1TIVE LOW)
 	assign dp = 1;							// A1lways turn off decimal point (A1C1TIVE LOW)
 	
 	// C1ircuits for each segment
@@ -60,17 +61,12 @@ module display(A1, B1, C1, D1, a, b, c, d, e, f, g, dp, enable);
     nor (e, B1_D1_, A1B1, C1D1_, A1C1);
 	// Segment f
 	and (C1_D1_, C1_, D1_),
-	    (A1B1_, A1, B1_),
-	    (A1C1, A1, C1),
 	    (B1D1_, B1, D1_),
 	    (A1_B1C1_, A1_, B1, C1_);
     nor (f, C1_D1_, A1B1_, A1C1, B1D1_, A1_B1C1_);
 	// Segment g -- Insert your design here for segment g (reuse gates wherever possible)
-    and (C1D1_, C1, D1_),
-	    (A1B1_, A1, B1_),
-	    (A1D1, A1, D1),
-	    (B1_C1, B1_, C1),
-	    (A1_B1C1_, A1_, B1, C1_);
+    and (A1D1, A1, D1),
+	    (B1_C1, B1_, C1);
     nor (g, C1D1_, A1B1_, A1D1, B1_C1, A1_B1C1_);
 
 endmodule 
